d7cd5b59b9167651c9f5fb4a55719f4cc84bbca4
Lxmonexe <93253891+Lxmonexe@users.noreply.github.com>
2024-06-05 11:22:40 +0200

    fix: add litle changes

--- frontend/js/view/cardsView.js
+++ frontend/js/view/cardsView.js
@@ -1,9 +1,13 @@
-export function generateCardGrid(){
-    const cards = document.querySelector("gridCards")
-    for(let i = 0; i < 25; i++){
-        const card = document.createElement("card") 
-        cards.appendChild(card)
+export class cardsView{
+    constructor(){
     }
-}
+    generateCardGrid(){
+        const cards = document.querySelector("gridCards")
+        for(let i = 0; i < 25; i++){
+            const card = document.createElement("card") 
+            cards.appendChild(card)
+        }
+    }
+} 
 
-window.addEventListener("load", generateCardGrid)
+
