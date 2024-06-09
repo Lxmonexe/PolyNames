f1edcd35e0686dd9ce2c18f9ff55bb9547b1d7df
aa14728 <arthur_albert@etu.u-bourgogne.fr>
2024-06-09 17:31:25 +0200

    main test with grille

--- backend/src/App.java
+++ backend/src/App.java
@@ -1,6 +1,7 @@
-import controllers.CartesController;
-import controllers.DictionnairesController;
-import dao.CartesDAO;
+import controllers.MotController;
+import dao.GrilleDAO;
+import dao.MotDAO;
+import models.Mot;
 import webserver.WebServer;
 import webserver.WebServerContext;
 
@@ -10,20 +11,20 @@ public class App {
         WebServer webServer = new WebServer();
         webServer.listen(8080);
 
-        CartesDAO cartesDAO = new CartesDAO();
-        cartesDAO.create();
+        // CartesDAO cartesDAO = new CartesDAO();
+        // cartesDAO.create();
+
+        GrilleDAO grilleDAO = new GrilleDAO();
+        grilleDAO.create(1);
 
 
-        webServer.getRouter().get("/carte", (WebServerContext context) -> { CartesController.findAll(context);});
-        // webServer.getRouter().post("/carte/create", (WebServerContext context) -> { CartesController.create(context);});
-        webServer.getRouter().get("/dictionnaire", (WebServerContext context) -> { DictionnairesController.findAll(context);});
-        webServer.getRouter().get("/partie", (WebServerContext context) -> { DictionnairesController.findAll(context);});
-        webServer.getRouter().get("/joueur", (WebServerContext context) -> { DictionnairesController.findAll(context);});
-        webServer.getRouter().get("/role", (WebServerContext context) -> { DictionnairesController.findAll(context);});
+        webServer.getRouter().get("/mots", (WebServerContext context) -> { MotController.findAll(context);}); //fonctionne
+        // webServer.getRouter().get("/grille", (WebServerContext context) -> { MotController.findAll(context);}); //fonctionne
+
         
-        webServer.getRouter().get("/create-polynames-or-join-polynames", (WebServerContext context) -> { DictionnairesController.findAll(context);});
-        webServer.getRouter().get("/polynames/role", (WebServerContext context) -> { DictionnairesController.findAll(context);});
-        webServer.getRouter().get("/polynames/lord-of-words", (WebServerContext context) -> { DictionnairesController.findAll(context);});
-        webServer.getRouter().get("/polynames/lord-of-intuitions", (WebServerContext context) -> { DictionnairesController.findAll(context);});
+        // webServer.getRouter().get("/create-polynames-or-join-polynames", (WebServerContext context) -> { DictionnairesController.findAll(context);});
+        // webServer.getRouter().get("/polynames/role", (WebServerContext context) -> { DictionnairesController.findAll(context);});
+        // webServer.getRouter().get("/polynames/lord-of-words", (WebServerContext context) -> { DictionnairesController.findAll(context);});
+        // webServer.getRouter().get("/polynames/lord-of-intuitions", (WebServerContext context) -> { DictionnairesController.findAll(context);});
     }
 }
