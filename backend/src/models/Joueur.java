bd8017a49210bb797d8893af161d27b17514c2d4
aa14728 <arthur_albert@etu.u-bourgogne.fr>
2024-06-09 17:30:56 +0200

    update models

--- backend/src/models/Joueur.java
+++ backend/src/models/Joueur.java
@@ -1,5 +1,20 @@
 package models;
 
 public class Joueur {
+    private int _id;
+    private String _pseudo;
+
+    public Joueur(int id, String pseudo) {
+        _id = id;
+        _pseudo = pseudo;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public String get_pseudo() {
+        return _pseudo;
+    }
 
 }
