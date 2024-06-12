edf7266e86d1df0115db8e0607202d2bac171550
aa14728 <arthur_albert@etu.u-bourgogne.fr>
2024-06-12 17:55:26 +0200

    upte

--- backend/src/controllers/GrilleController.java
+++ backend/src/controllers/GrilleController.java
@@ -2,17 +2,21 @@ package controllers;
 
 import java.sql.SQLException;
 
+import dao.GrilleDAO;
 // import dao.GrilleDAO;
 import webserver.WebServerContext;
 
 public class GrilleController {
 
-    public static void findAll(WebServerContext context){
-        // GrilleDAO grilleDAO = new GrilleDAO();
-        // try {
-        //     context.getResponse().json(grilleDAO.findAll());
-        // } catch (SQLException e) {
-        //     context.getResponse().send(500, "Internal Server Error: " + e.getMessage());
-        // }
+
+    public static void create(WebServerContext context){
+        GrilleDAO grilleDAO = new GrilleDAO();
+        try {
+            grilleDAO.create(context.getRequest().getParam("id"));
+            context.getResponse().json(grilleDAO.findAll(context.getRequest().getParam("id")));
+            context.getResponse().ok("Grille ok");
+        } catch (SQLException e) {
+            context.getResponse().send(500, "Internal Server Error: " + e.getMessage());
+        }
     }
 }
