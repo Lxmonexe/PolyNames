7b9fbb063c18b5d62d19957e3c5fd4aea8104d1c
Lxmonexe <93253891+Lxmonexe@users.noreply.github.com>
2024-06-14 22:35:21 +0200

    refactor: Update App.java to add new routes for creating and managing game sessions

--- backend/src/App.java
+++ backend/src/App.java
@@ -13,25 +13,24 @@ public class App {
         WebServer webServer = new WebServer();
         webServer.listen(8080);
 
-        // CartesDAO cartesDAO = new CartesDAO();
-        // cartesDAO.create();
 
-        // GrilleDAO grilleDAO = new GrilleDAO();
-        // grilleDAO.create("#2c5b");
+        webServer.getRouter().put("/partie/code/grille/:partieid", (WebServerContext context) -> { GrilleController.getGrille(context); });
 
 
         webServer.getRouter().post("/partie/code/:partieid", (WebServerContext context) -> { PartieController.createPartie(context); });
+        
         webServer.getRouter().post("/partie/creer/joueur/:partieid/:pseudo/:role", (WebServerContext context) -> { JoueurController.createJoueur(context); ParticiperController.createParticipant(context); });
+        
         webServer.getRouter().post("/partie/code/creer/grille/:partieid", (WebServerContext context) -> { GrilleController.create(context); });
-        webServer.getRouter().put("/partie/code/grille/:partieid", (WebServerContext context) -> { GrilleController.getGrille(context); });
+        
         webServer.getRouter().post("/partie/creer/nouvelle-partie/:partieid/:pseudo/:role", (WebServerContext context) -> {ParticiperController.createParticipant(context); });
-
+        
         webServer.getRouter().post("/partie/code/score/:partieid/:score", (WebServerContext context) -> { 
             ParticiperController.updateScore(context); 
             int score = ParticiperController.getScore(context);
             webServer.getSSE().emit("score", "{ \"scorePartie\": \"" + score + "\" }");
         });
-
+        
         webServer.getRouter().post("/partie/indice/:mot/:nbcarte", (WebServerContext context) -> { 
             WebServerResponse response = context.getResponse();
             String mot = context.getRequest().getParam("mot");
@@ -67,12 +66,5 @@ public class App {
             String mot = context.getRequest().getParam("mot");
             webServer.getSSE().emit("decouvert", "{ \"mot\": \"" + mot + "\" }");
          });
-
-
-        // webServer.getRouter().get("/partie/:code", (WebServerContext context) -> { ParticiperController.getRoleById(context);}); //fonctionne
-        // webServer.getRouter().get("/partie/:code:/:joueurid/", (WebServerContext context) -> { ParticiperController.getRoleById(context);}); //fonctionne
-        // webServer.getRouter().get("/partie/:code/:joueurid/:role", (WebServerContext context) -> { GrilleController.findAll(context);}); //fonctionne
-        
-        // webServer.getRouter().get("/mots", (WebServerContext context) -> { MotController.findAll(context);}); //fonctionne
     }
 }
